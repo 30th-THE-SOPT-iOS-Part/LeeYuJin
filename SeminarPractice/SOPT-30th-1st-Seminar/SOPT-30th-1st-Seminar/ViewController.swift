2ebfce60c3d41d50c53112f313100129434a9182
Yujin Lee <yj587878@gmail.com>
2022-04-02 15:58:03 +0900

    [FEAT] 버튼 누르면 라벨 바뀌는 기능 추가

--- SeminarPractice/SOPT-30th-1st-Seminar/SOPT-30th-1st-Seminar/ViewController.swift
+++ SeminarPractice/SOPT-30th-1st-Seminar/SOPT-30th-1st-Seminar/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func moneyButtonDidTap(_ sender: Any) {
+        messageLabel.text = "새로운 메세지가 도착했어요"
+        messageLabel.textColor = UIColor.green
+        messageLabel.sizeToFit()
+        AudioServicesPlayAlertSound(SystemSoundID(1307))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel.text = "도착한 메세지가 없어요!"
+        
+        
     }
 
 
