a33b36042de9900c0e27766d93ee27590ce86a54
Yujin Lee <yj587878@gmail.com>
2022-04-11 01:37:59 +0900

    [FEAT] 심화과제 기능 구현

--- Instagram-Clone/Instagram-Clone/Source/Extension/UITextField++.swift
+++ Instagram-Clone/Instagram-Clone/Source/Extension/UITextField++.swift
@@ -13,11 +13,12 @@ extension UITextField {
     self.leftView = paddingView
     self.leftViewMode = ViewMode.always
   }
-
-    func setLeftPadding(width: CGFloat){
-        self.leftView?.snp.updateConstraints{ make in
-            make.width.equalTo(width)
-        }
+    func addRightPadding(width : CGFloat = 10) {
+      let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+      self.rightView = paddingView
+      self.rightViewMode = ViewMode.always
     }
+
+
 }
 
--- Instagram-Clone/Instagram-Clone/Source/Views/Components/AuthTextField.swift
+++ Instagram-Clone/Instagram-Clone/Source/Views/Components/AuthTextField.swift
@@ -14,6 +14,9 @@ class AuthTextField: UITextField {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    private lazy var visibleButton = UIButton().then{
+        $0.setImage(UIImage(named: "password hidden eye icon"), for: .normal)
+    }
     
     init(type: textFieldType, placeHolder: String) {
         super.init(frame: .zero)
@@ -26,6 +29,7 @@ class AuthTextField: UITextField {
         case .password:
             self.isSecureTextEntry = true
             self.placeholder = placeHolder
+            setVisibleButton()
         }
     }
     
@@ -39,5 +43,30 @@ class AuthTextField: UITextField {
             $0.height.equalTo(40)
         }
     }
+    
+    func setVisibleButton(){
+        self.addRightPadding(width: 30)//버튼이 가려지지 않게 오른쪽 패딩 추가
+        
+        self.addSubview(visibleButton)
+        visibleButton.snp.makeConstraints{
+            $0.trailing.equalToSuperview().offset(-10)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(20)
+        }
+       
+        
+        visibleButton.addTarget(self, action:#selector(visibleButtonDidTap(_:)), for: .touchUpInside)
+    }
+    
+    @objc private func visibleButtonDidTap(_ sender: UIButton){
+        isSecureTextEntry.toggle()
+        switch isSecureTextEntry{
+        case true:
+            visibleButton.setImage(UIImage(named: "password hidden eye icon"), for: .normal)
+        case false:
+            visibleButton.setImage(UIImage(named: "password shown eye icon"), for: .normal)
+        }
+        
+     }
 
 }
