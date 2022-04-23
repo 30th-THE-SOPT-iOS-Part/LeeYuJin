c9f15d2c01353f92b9137030b898b771bcb68920
Yujin Lee <yj587878@gmail.com>
2022-04-23 15:12:01 +0900

    [FEAT] 스크롤뷰 예제 구현

---
+++ SeminarPractice/Sopt-3rd-Seminar/Sopt-3rd-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt-3rd-Seminar
+//
+//  Created by 이유진 on 2022/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
